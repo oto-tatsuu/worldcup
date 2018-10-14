bf6cd492c758e13b2c9db56af2501be95e18946e
tatsuu <1478758945@q.com>
2018-10-14 11:48:01 +0800

    com list

--- worldcup/worldcup/List.h
+++ worldcup/worldcup/List.h
@@ -26,11 +26,11 @@ private:
 	ListNode<T>*ptr;
 public:
 	List_iter(ListNode<T>*ptr) :ptr(ptr) {};
-	List_iter(List_iter<T>&src) :ptr(src) {};
 	~List_iter() { ptr = nullptr; };
-	List_iter<T>&operator =(List_iter<T>&src);
 	bool operator ==(List_iter<T>&src);
+	bool operator ==(List_iter<T>&&src);
 	bool operator !=(List_iter<T>&src);
+	bool operator !=(List_iter<T>&&src);
 	List_iter<T>& operator ++();
 	List_iter<T>& operator ++(int);
 	T operator *();
@@ -54,256 +54,22 @@ public:
 	void assign(List_iter<T> begin, List_iter<T> end);
 	void clear();
 	bool empty();
+	T back();
+	T front();
 	void push_back(T data);
 	void push_front(T data);
 	void pop_back();
 	void pop_front();
 	void remove(T data);
+	void remove(List_iter<T> fore_iter);
+	bool search(T data);
 	void insert(int index,T data);
-	int Size();
-	void Show();
-
-	
+	int size();
+	//void show();
+private:
+	List_iter<T> search_foreiter(T data);
+	List_iter<T> index_foreiter(int index);
 };
-////
-//template<typename T>
-//inline ListNode<T>::ListNode()
-//{
-//}
-////
-//template<typename T>
-//inline ListNode<T>::ListNode(T data, ListNode<T>* next) :data(data), next(next)
-//{
-//}
-////
-//template<typename T>
-//inline ListNode<T>::~ListNode()
-//{
-//	if (next)
-//	{
-//		delete next;
-//		next = nullptr;
-//	}
-//	std::cout << "xigou";
-//}
-//
-//template<typename T>
-//inline List<T>::List()
-//{
-//	first = iterator(new ListNode<T>());
-//	last = first;
-//	size = 0;
-//}
-//
-//template<typename T>
-//inline List<T>::List(List<T>& src)
-//{
-//	first = iterator(new ListNode<T>());
-//	assign(src.begin(), src.end());
-//}
-//
-//template<typename T>
-//inline List<T>::~List()
-//{
-//	clear();
-//	delete first;
-//	first = nullptr;
-//	last = nullptr;
-//}
-//
-//template<class T>
-//inline List<T>::iterator List<T>::begin()
-//{
-//	return iterator();
-//}
-//
-//
-//
-//template<class T>
-//inline List<T>::iterator List<T>::end()
-//{
-//	return last++;
-//}
-//
-//
-//
-//template<typename T>
-//inline List<T>& List<T>::operator=(List & rhs)
-//{
-//	// TODO: 在此处插入 return 语句
-//	assign(rhs.begin(),rhs.end());
-//	return this;
-//}
-//
-//template<typename T>
-//inline void List<T>::assign(List<T>::iterator first, List<T>::iterator last)
-//{
-//	clear();
-//	List<T>::iterator iter;
-//	while (iter != end()) 
-//	{
-//		push_back(*iter);
-//		iter++;
-//	}
-//}
-//
-//
-//template<typename T>
-//inline bool List<T>::empty()
-//{
-//	return size==0;
-//}
-//
-//template<typename T>
-//inline void List<T>::push_back(T data)
-//{
-//	ListNode<T>*p = new ListNode<T>(data);
-//	last->next = p;
-//	last = p;
-//	size++;
-//}
-//
-//template<typename T>
-//inline void List<T>::push_front(T data)
-//{
-//	ListNode<T>*p = new ListNode<T>(data);
-//	p->next = first->next;
-//	first->next = p;
-//	size++;
-//}
-//
-//template<typename T>
-//inline void List<T>::pop_back()
-//{
-//	ListNode<T>*p = first->next;
-//	while (p->next!=last) 
-//	{
-//		p=p->next
-//	}
-//	delete p->next;
-//	last = p;
-//	size--;
-//}
-//
-//template<typename T>
-//inline void List<T>::pop_front()
-//{
-//	ListNode<T>*p = first->next;
-//	first - next = p->next;
-//	p->next = nullptr;
-//	delete p;
-//	size--;
-//}
-//
-//
-//template<typename T>
-//inline void List<T>::remove(T data)
-//{
-//	ListNode<T>*p = first, *q;
-//	while (p->next&&p->next->data != data)
-//		p = p->next;
-//	q = p->next;
-//	if (!q)
-//		return;
-//	if (q == last)
-//		last = p;
-//	p->next = q->next;
-//	q->next = nullptr;
-//	delete q;
-//	size--;
-//}
-//
-//template<typename T>
-//inline void List<T>::insert(int index,T data)
-//{
-//	if (index > size)
-//		return;
-//	ListNode<T>*p = first;
-//	for (int count = 0; count < index; count++)
-//		p = p->next;
-//	p->next = new ListNode<T>(data, p->next);
-//	size++;
-//	if (last == p)
-//		last = p->next;
-//}
-//
-//template<typename T>
-//inline int List<T>::Size()
-//{
-//	return size;
-//}
-//
-//template<typename T>
-//inline void List<T>::clear()
-//{
-//	delete first->next;
-//	first->next = nullptr;
-//	size = 0;
-//	last = first;
-//}
-//
-//template<typename T>
-//inline void List<T>::Show()
-//{
-//	ListNode<T>*p = first->next;
-//	while (p)
-//	{
-//		std::cout << p->data;
-//		p = p->next;
-//	}
-//}
-//
-////
-//template<typename T>
-//inline List<T>::iterator::iterator(ListNode<T>* ptr):ptr(ptr)
-//{
-//}
-//template<class T>
-//inline List<T>::iterator::iterator(List<T>::iterator& src) : ptr(src.ptr)
-//{
-//}
-////
-//
-////
-//template<typename T>
-//inline List<T>::iterator::~iterator()
-//{
-//	delete ptr;
-//}
-////
-//template<class T>
-//inline bool List<T>::iterator::operator==(List<T>::iterator& src)
-//{
-//	return false;
-//}
-////
-//template<typename T>
-//inline bool List<T>::iterator::operator!=(List<T>::iterator& src)
-//{
-//	return ptr != src.ptr;
-//}
-////
-//template<class T>
-//inline List<T>::iterator List<T>::iterator::operator++()
-//{
-//	if (ptr)
-//		ptr = ptr.next;
-//	else
-//		throw runtime_error("链表迭代器越界");
-//	return this;
-//}
-////
-//template<typename T>
-//inline T List<T>::iterator::operator*()
-//{
-//	return ptr->data;
-//}
-////
-//template<typename T>
-//inline ListNode<T>* List<T>::iterator::operator->()
-//{
-//	return ptr;
-//}
 
 template<typename T>
 inline ListNode<T>::ListNode()
@@ -318,15 +84,9 @@ inline ListNode<T>::ListNode(T data, ListNode<T>* next):data(data),next(next)
 template<typename T>
 inline ListNode<T>::~ListNode()
 {
+	std::cout << "xigou" << std::endl;
 }
 
-template<typename T>
-inline List_iter<T> & List_iter<T>::operator=(List_iter<T> & src)
-{
-	// TODO: 在此处插入 return 语句
-	ptr = src.ptr;
-	return this;
-}
 
 template<typename T>
 inline bool List_iter<T>::operator==(List_iter<T>&src)
@@ -335,7 +95,19 @@ inline bool List_iter<T>::operator==(List_iter<T>&src)
 }
 
 template<typename T>
+inline bool List_iter<T>::operator==(List_iter<T>&& src)
+{
+	return ptr==src.ptr;
+}
+
+template<typename T>
 inline bool List_iter<T>::operator!=(List_iter<T>&src)
+{
+	return ptr!=src.ptr;
+}
+
+template<typename T>
+inline bool List_iter<T>::operator!=(List_iter<T>&& src)
 {
 	return ptr!=src.ptr;
 }
@@ -382,7 +154,7 @@ inline List<T>::List(List<T>& src)
 {
 	first = new ListNode<T>();
 	last = first;
-	assign(src.begin(), src.end());
+	assign(src.begin(),src.end() );
 }
 
 template<class T>
@@ -398,7 +170,7 @@ template<class T>
 inline List<T>& List<T>::operator=(List & rsh)
 {
 	// TODO: 在此处插入 return 语句
-	//assign(rsh.begin(), rsh.end());
+	assign(rsh.begin(), rsh.end());
 	return *this;
 }
 
@@ -446,9 +218,129 @@ inline bool List<T>::empty()
 }
 
 template<class T>
+inline T List<T>::back()
+{
+	return last->data;
+}
+
+template<class T>
+inline T List<T>::front()
+{
+	return first->next->data;
+}
+
+template<class T>
 inline void List<T>::push_back(T data)
 {
 	last->next = new ListNode<T>(data);
 	last = last->next;
 	size++;
 }
+
+template<class T>
+inline void List<T>::push_front(T data)
+{
+	ListNode<T>*p=new ListNode<T>(data,first->next);
+	if (!first->next)
+		last = p;
+	first->next = p;
+	size++;
+}
+
+template<class T>
+inline void List<T>::pop_back()
+{
+	ListNode<T>*p = first;
+	for (; p->next != last; p = p->next);
+	delete p->next;
+	p->next = nullptr;
+	size--;
+	last = p;
+}
+
+template<class T>
+inline void List<T>::pop_front()
+{
+	ListNode<T>*p = first->next;
+	first->next = p->next;
+	if (p == last)
+		last = first;
+	delete p;
+	size--;
+	p = nullptr;
+}
+
+template<class T>
+inline void List<T>::remove(T data)
+{
+	List_iter<T> p = search_foreiter(data);
+	if (p.ptr != nullptr)
+		remove(p);
+}
+
+template<class T>
+inline void List<T>::remove(List_iter<T> fore_iter)
+{
+	ListNode<T>*p = fore_iter->next;
+	fore_iter->next = p->next;
+	if (p == last)
+		last = fore_iter.ptr;
+	delete p;
+	size--;
+	p = nullptr;
+	
+}
+
+template<class T>
+inline bool List<T>::search(T data)
+{
+	ListNode<T>*p = first->next;
+	while (p) 
+	{
+		if (p->data == data)
+			return true;
+		p = p->next;
+	}
+	return false;
+}
+
+template<class T>
+inline void List<T>::insert(int index, T data)
+{
+	List_iter<T>iter=index_foreiter(index);
+	iter->next = new ListNode<T>(data, iter->next);
+	size++;
+	if (iter.ptr == last)
+		last = iter->next;
+}
+
+template<class T>
+inline int List<T>::size()
+{
+	return size;
+}
+
+template<class T>
+inline List_iter<T> List<T>::search_foreiter(T data)
+{
+	ListNode<T>*p = first;
+	while (p)
+	{
+		if (p->next->data == data)
+			return List_iter<T>(p);
+		p = p->next;
+	}
+	
+	return List_iter<T>(nullptr);
+}
+
+template<class T>
+inline List_iter<T> List<T>::index_foreiter(int index)
+{
+	ListNode<T>*p = first;
+	if (index > size)
+		throw std::runtime_error("下标越界");
+	for (int i = 0; i < index; p = p->next, i++);
+	return List_iter<T>(p);
+}
+
--- worldcup/worldcup/源.cpp
+++ worldcup/worldcup/源.cpp
@@ -7,8 +7,12 @@ int main()
 	List<int>l;
 	List<int>a;
 	for (int i = 0; i < 10; i++)
-		l.push_back(i);
-	a = l;
-	List<int>b = a;
+		l.push_front(i);
+	l.pop_front();
+	l.pop_front();
+	l.insert(5, 9);
+	l.insert(9, 8);
+	for (List_iter<int> p = l.begin(); p != l.end(); p++)
+		std::cout << *p<<std::endl;
 	return 0;
 }
